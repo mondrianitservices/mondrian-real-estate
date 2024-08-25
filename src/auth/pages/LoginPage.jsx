dda2200a9876db7303416bc5277443dd74fab7ea
Aaron Obed Giron <aogarrido.g@gmail.com>
2024-08-25 14:17:08 -0600

    Cambio: Mejorar el Login con joy Material

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,52 +1,204 @@
 import { Link as RouterLink} from 'react-router-dom'
-import {Grid,Typography,TextField,Button,Link} from '@mui/material';
-// import {Google} from '@mui/icons-material';
-import {GoogleIcon} from './GoogleIcon'
-import { AuthLayout } from '../layout/AuthLayout';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Checkbox from '@mui/joy/Checkbox';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import { IconButton } from '@mui/joy';
+import Link from '@mui/joy/Link';
+import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
+import Stack from '@mui/joy/Stack';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
+import GoogleIcon from './GoogleIcon';
+
+
+function ColorSchemeToggle() {
+  return (
+    <IconButton
+      aria-label="toggle light/dark mode"
+      size="sm"
+      variant="outlined"  
+    >
+      {/* Temas */}
+      <DarkModeRoundedIcon/>
+    </IconButton>
+  );
+}
 
 export const LoginPage = () => {
   return (
-    <AuthLayout title="Login">
-                <form>
-            <Grid container>
-              <Grid item xs={12} sx={{mt:2}}>
-                <TextField
-                  label="Correo"
-                  type="email"
-                  placeholder='correo'
-                  fullWidth
-                />                
-              </Grid>
-              <Grid item xs={12} sx={{mt:2}}>
-                <TextField
-                  label="Contrasena"
-                  type="password"
-                  placeholder='Contrasena'
-                  fullWidth
-                />                
-              </Grid>
-
-              {/* Botones de login */}
-              <Grid container spacing={2} sx={{mb:2,mt:1}}>
-                <Grid item xs={12} sm={6}>
-                  <Button variant="contained" fullWidth>
-                  Login
+    <CssVarsProvider>
+      <CssBaseline/>
+      <GlobalStyles
+        styles={{
+          ':root': {
+            '--Form-maxWidth': '800px',
+            '--Transition-duration': '0.4s', // set to `none` to disable transition
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          width: { xs: '100%', md: '50vw' },
+          transition: 'width var(--Transition-duration)',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          position: 'relative',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backdropFilter: 'blur(12px)',
+          backgroundColor: 'rgba(255 255 255 / 0.2)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundColor: 'rgba(19 19 24 / 0.4)',
+          },
+        })}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100dvh',
+            width: '100%',
+            px: 2,
+          }}
+        >
+          <Box
+            component="header"
+            sx={{
+              py: 3,
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+              <IconButton variant="soft" color="primary" size="sm">
+                <BadgeRoundedIcon />
+              </IconButton>
+              <Typography level="title-lg">Mondrian Logo</Typography>
+            </Box>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: 'auto',
+              py: 2,
+              pb: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              width: 400,
+              maxWidth: '100%',
+              mx: 'auto',
+              borderRadius: 'sm',
+              '& form': {
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              },
+              [`& .MuiFormLabel-asterisk`]: {
+                visibility: 'hidden',
+              },
+            }}
+          >
+            <Stack gap={4} sx={{ mb: 2 }}>
+              <Stack gap={1}>
+                <Typography component="h1" level="h3">
+                  Ingreso
+                </Typography>
+                <Typography level="body-sm">
+                  ¿Aun no tienes Cuenta?{' '}
+                  <Link component={RouterLink} to="/auth/register">
+                  ¡Registrate!
+                  </Link>
+                </Typography>
+              </Stack>
+              <Button
+                variant="soft"
+                color="neutral"
+                fullWidth
+                startDecorator={<GoogleIcon />}
+              >
+                Ingresar con Cuenta de Google
+              </Button>
+            </Stack>
+            <Divider
+              sx={(theme) => ({
+                [theme.getColorSchemeSelector('light')]: {
+                  color: { xs: '#FFF', md: 'text.tertiary' },
+                },
+              })}
+            >
+              or
+            </Divider>
+            <Stack gap={4} sx={{ mt: 2 }}>
+              <form
+              >
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" name="password" />
+                </FormControl>
+                <Stack gap={4} sx={{ mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Checkbox size="sm" label="Remember me" name="persistent" />
+                    <Link level="title-sm" href="#replace-with-a-link">
+                      Olvidaste tu contrasena?
+                    </Link>
+                  </Box>
+                  <Button type="submit" fullWidth>
+                    Ingresar
                   </Button>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Button variant="contained" fullWidth>
-                    <GoogleIcon />
-                    <Typography sx={{ml:1}}>Google</Typography>
-                  </Button>
-                </Grid>
-              </Grid>
-              <Grid container direction='row' justifyContent='end'>
-                <Link component={RouterLink} color='inherit' to="/auth/register">
-                  Crear una cuenta
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-    </AuthLayout>
+                </Stack>
+              </form>
+            </Stack>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © Mondrian IT Services {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: '100%',
+          position: 'fixed',
+          right: 0,
+          top: 0,
+          bottom: 0,
+          left: { xs: 0, md: '50vw' },
+          transition:
+            'background-image var(--Transition-duration), left var(--Transition-duration) !important',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          backgroundColor: 'background.level1',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage:
+            'url(https://media.istockphoto.com/id/1188112158/es/foto/vista-panor%C3%A1mica-de-bogot%C3%A1-colombia.webp?s=1024x1024&w=is&k=20&c=cKd4E_Eyp7ruV2eFn7xXA7dpcckKEvyT1y_3E42FQc4=)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundImage:
+              'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)',
+          },
+        })}
+      />
+    </CssVarsProvider>
   )
 }
