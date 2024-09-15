c221ba7b4898434b84b96e85cd67c1204795a46d
Aaron Obed Giron <aogarrido.g@gmail.com>
2024-09-14 18:47:14 -0600

    Cambio: Solventar problema de estilos que hacia que los view esten separados del sidebar.

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,25 +1,20 @@
 import { Box } from "@mui/joy"
-import {SidebarMenu, Header} from "../components"
+import { SidebarMenu, Header } from "../components"
 import { AppTheme } from "../../theme/AppTheme";
 
 const drawerWidth = 280;
 
-export const JournalLayout = ({children}) => {
+export const JournalLayout = ({ children }) => {
   return (
     <AppTheme>
-      <Box sx={{display:'flex', minHeight: '100dvh'}}>
-      <Header/>
-      <SidebarMenu/>
+      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+        <Header />
+        <SidebarMenu />
         <Box
           component="main"
           className="MainContent"
           sx={{
-            px: { xs: 2, md: 6 },
-            pt: {
-              xs: 'calc(12px + var(--Header-height))',
-              sm: 'calc(12px + var(--Header-height))',
-              md: 3,
-            },
+            pt: { xs: 'calc(12px + var(--Header-height))', md: 3 },
             pb: { xs: 2, sm: 2, md: 3 },
             flex: 1,
             display: 'flex',
@@ -27,12 +22,13 @@ export const JournalLayout = ({children}) => {
             minWidth: 0,
             height: '100dvh',
             gap: 1,
+            overflow: 'auto',
           }}
         >
-            {children}
+          {children}
         </Box>
-    </Box>
+      </Box>
     </AppTheme>
-    
+
   )
 }
